be1866b41e0d333a433d7781ad17ce33ae838296
Анастасия <anastasiaelhova@gmail.com>
2022-04-25 00:43:27 +0300

    Добавляет onSubmit на форму регистрации

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -17,8 +17,18 @@ const RegisterForm: FC = () => {
   });
 
   const onSubmit = (data: object) => {
-    console.log(data)
-  }
+    let xhr = new XMLHttpRequest();
+    let url = "http://localhost:8085/registration";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(this.responseText);
+      }
+    };
+    let dataJson = JSON.stringify(data);
+    xhr.send(dataJson);
+  };
 
   return (
     <main className="form-signin">
