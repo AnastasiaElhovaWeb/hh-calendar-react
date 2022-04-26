65888c62d18c81547d6d31cdfed47cb1b3f3b5e7
elhova.ad <elhova.ad@lsrgroup.ru>
2022-04-26 11:40:29 +0300

    добавляет вывод ответов от "сервера"

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './assets/css/core.css';
 import './assets/css/main.css';
 import RegisterForm from "./components/RegisterForm";
+//import LoginForm from "./components/LoginForm";
 
 
 ReactDOM.render(<RegisterForm />, document.getElementById('root'));
